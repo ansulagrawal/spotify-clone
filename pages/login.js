bcbee07035f3cf3d73b3f39c35f49e6c61c5212e
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-12-11 22:54:47 +0530

    Customising the Signin Page

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,25 @@
-import { getProviders } from 'next-auth/react';
+import { getProviders, signIn } from 'next-auth/react';
 
 const Login = (props) => {
-   return <div></div>;
+   return (
+      <div className='flex flex-col justify-center min-h-screen w-full items-center bg-[#1c1c1c]'>
+         <img
+            className='w-52 mb-5'
+            src='https://links.papareact.com/9xl'
+            alt='Spotify Logo'
+         />
+         {Object.values(props.providers).map((provider) => (
+            <div key={provider.name}>
+               <button
+                  className='bg-[#18D860] text-white p-5 rounded-3xl'
+                  onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+               >
+                  Login with {provider.name}
+               </button>
+            </div>
+         ))}
+      </div>
+   );
 };
 
 export default Login;
